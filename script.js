abbfeba5c809d96f3daf495d7e971783ed0bf67b
specter <mikexx1264@gmail.com>
2020-02-14 16:24:12 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// var usuario = prompt("Elige tu regalo sorpresa!, desea continuar?");
+var regalos =  parseInt(prompt("Elige un regalo\nregalo 1, regalo 2, regalo 3"));
+
+if (regalos == 1) {
+  document.write("Tu regalo es! " + "Cena " + "Te amo");
+} else if (regalos == 2) {
+  document.write("Tu regalo es! " + "Chocolates " + "Te amo");
+} else if (regalos == 3) {
+  document.write("Tu regalo es! " + "Cocholates Bones y Peliculas! " + " Te amo");
+} else {
+  document.write("Todo Mi Amor!! jaja Me haces tan feliz");
+}
